20b443a389da934827724fbfde87567a412aa4fd
Canberk Temiz <canberk.tmz@gmail.com>
2023-04-26 14:04:36 +0200

    create custom fetch hook

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,7 +1,17 @@
-<script setup>
-
-</script>
-
 <template>
     <h1>Products Page</h1>
+    <ul>
+        <li v-for="product in data" :key="product.iu">
+            {{ product.name }}
+        </li>
+    </ul>
 </template>
+
+<script setup>
+import useProducts from '@/composable/useProducts.js'
+
+const { data, fetchProducts } = useProducts();
+
+fetchProducts();
+
+</script>
