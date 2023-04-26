229822651a3a5f11a9a42cf9b4108ceafaa62fad
Canberk Temiz <canberk.tmz@gmail.com>
2023-04-26 13:43:36 +0200

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ProductView from '../views/ProductView.vue'
+import CartView from '../views/CartView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/products',
+      name: 'products',
+      component: ProductView
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView
+    },
+  ]
+})
+
+export default router
